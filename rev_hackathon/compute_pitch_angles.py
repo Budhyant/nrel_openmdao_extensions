51dcb5976572c4703a93af1c74e4fc349dfcb1f9
John Jasa <johnjasa11@gmail.com>
2020-10-05 13:01:47 -0600

    Added MDF and sequential opts

--- rev_hackathon/compute_pitch_angles.py
+++ rev_hackathon/compute_pitch_angles.py
@@ -3,14 +3,14 @@ from scipy.optimize import minimize
 import openmdao.api as om
 
 
-def compute_power(pitch_angle, wind_speed, airfoil_design):
-    CD = np.pi * airfoil_design * np.deg2rad(pitch_angle) ** 2
-    airfoil_power_boost = (airfoil_design - wind_speed * 2.0) ** 2.0 / 10.0
+def compute_power(pitch_angle, wind_speed, drag_modifier):
+    CD = np.pi * drag_modifier * np.deg2rad(pitch_angle) ** 2
+    airfoil_power_boost = (drag_modifier - wind_speed * 2.0) ** 2.0 / 10.0
     return -((wind_speed - CD) ** 3) - airfoil_power_boost
 
 
-def compute_power_constraint(pitch_angle, wind_speed, airfoil_design):
-    neg_power = compute_power(pitch_angle, wind_speed, airfoil_design)
+def compute_power_constraint(pitch_angle, wind_speed, drag_modifier, P_rated):
+    neg_power = compute_power(pitch_angle, wind_speed, drag_modifier)
     return neg_power + P_rated
 
 
@@ -23,7 +23,7 @@ class ComputePitchAngles(om.ExplicitComponent):
         size = self.options["size"]
 
         self.add_input("wind_speeds", np.zeros(size))
-        self.add_input("airfoil_design", 11.0)
+        self.add_input("drag_modifier", 11.0)
 
         self.add_output("pitch_angles", np.zeros(size))
         self.add_output("powers", np.zeros(size))
@@ -31,51 +31,52 @@ class ComputePitchAngles(om.ExplicitComponent):
 
     def compute(self, inputs, outputs):
         P_rated = self.options["P_rated"]
-        airfoil_design = inputs["airfoil_design"]
+        drag_modifier = inputs["drag_modifier"]
 
         for i, wind_speed in enumerate(inputs["wind_speeds"]):
             constraints = [
                 {
                     "type": "ineq",
                     "fun": compute_power_constraint,
-                    "args": [wind_speed, airfoil_design],
+                    "args": [wind_speed, drag_modifier, P_rated],
                 }
             ]
-            res = minimize(
+            result = minimize(
                 compute_power,
                 1.0,
-                args=(wind_speed, airfoil_design),
+                args=(wind_speed, drag_modifier),
                 method="SLSQP",
                 bounds=[(-15.0, 15.0)],
                 options={"disp": False},
                 constraints=constraints,
             )
-            outputs["pitch_angles"][i] = res.x
-            outputs["powers"][i] = res.fun
+            outputs["pitch_angles"][i] = result.x
+            outputs["powers"][i] = result.fun
 
         outputs["total_power"] = np.sum(outputs["powers"])
 
 
-wind_speeds = [4.0, 6.0, 8.0, 10.0]
-P_rated = 500.0
+if __name__ == "__main__":
+    wind_speeds = [4.0, 6.0, 8.0, 10.0]
+    P_rated = 500.0
 
-prob = om.Problem()
-prob.model.add_subsystem(
-    "compute_pitch_angles",
-    ComputePitchAngles(size=len(wind_speeds), P_rated=P_rated),
-    promotes=["*"],
-)
+    prob = om.Problem()
+    prob.model.add_subsystem(
+        "compute_pitch_angles",
+        ComputePitchAngles(size=len(wind_speeds), P_rated=P_rated),
+        promotes=["*"],
+    )
 
-prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options["optimizer"] = "SLSQP"
-prob.model.approx_totals(method="fd")
+    prob.driver = om.ScipyOptimizeDriver()
+    prob.driver.options["optimizer"] = "SLSQP"
+    prob.model.approx_totals(method="fd")
 
-prob.model.add_design_var("airfoil_design", lower=6.0, upper=15.0)
-prob.model.add_objective("total_power")
+    prob.model.add_design_var("drag_modifier", lower=6.0, upper=15.0)
+    prob.model.add_objective("total_power")
 
-prob.setup()
+    prob.setup()
 
-prob.set_val("wind_speeds", wind_speeds)
-prob.run_driver()
+    prob.set_val("wind_speeds", wind_speeds)
+    prob.run_driver()
 
-prob.model.list_outputs(print_arrays=True)
+    prob.model.list_outputs(print_arrays=True)
--- rev_hackathon/design_airfoil.py
+++ rev_hackathon/design_airfoil.py
@@ -11,7 +11,7 @@ class DesignAirfoil(om.ExplicitComponent):
         size = self.options["size"]
 
         self.add_input("wind_speeds", np.zeros(size))
-        self.add_input("airfoil_design", 10.0)
+        self.add_input("airfoil_design", 11.0)
 
         self.add_output("aerodynamic_efficiency", np.zeros(size))
         self.add_output("summed_efficiency")
@@ -21,32 +21,33 @@ class DesignAirfoil(om.ExplicitComponent):
 
         for i, wind_speed in enumerate(inputs["wind_speeds"]):
             lift = (airfoil_design - 7) * wind_speed
-            drag = (airfoil_design * wind_speed)**2
-            outputs['aerodynamic_efficiency'][i] = lift / drag
+            drag = (airfoil_design * wind_speed) ** 2
+            outputs["aerodynamic_efficiency"][i] = lift / drag
 
         outputs["summed_efficiency"] = -np.sum(outputs["aerodynamic_efficiency"])
 
 
-wind_speeds = [4.0, 6.0, 8.0, 10.0]
+if __name__ == "__main__":
+    wind_speeds = [4.0, 6.0, 8.0, 10.0]
 
-prob = om.Problem()
-prob.model.add_subsystem(
-    "design_airfoil",
-    DesignAirfoil(size=len(wind_speeds)),
-    promotes=["*"],
-)
+    prob = om.Problem()
+    prob.model.add_subsystem(
+        "design_airfoil",
+        DesignAirfoil(size=len(wind_speeds)),
+        promotes=["*"],
+    )
 
-prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options["optimizer"] = "SLSQP"
-prob.model.approx_totals(method="fd")
+    prob.driver = om.ScipyOptimizeDriver()
+    prob.driver.options["optimizer"] = "SLSQP"
+    prob.model.approx_totals(method="fd")
 
-prob.model.add_design_var("airfoil_design", lower=6.0, upper=15.0)
-prob.model.add_objective("summed_efficiency")
+    prob.model.add_design_var("airfoil_design", lower=6.0, upper=15.0)
+    prob.model.add_objective("summed_efficiency")
 
-prob.setup()
+    prob.setup()
 
-prob.set_val("wind_speeds", wind_speeds)
-prob.run_driver()
+    prob.set_val("wind_speeds", wind_speeds)
+    prob.run_driver()
 
-prob.model.list_inputs()
-prob.model.list_outputs(print_arrays=True)
+    prob.model.list_inputs()
+    prob.model.list_outputs(print_arrays=True)
